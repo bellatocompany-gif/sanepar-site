1b7de3e4e014bb287ed06efb4c35a26020e976a9
bellatocompany-gif <bellatocompany@gmail.com>
2026-08-12 13:15:49 -0300

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,22 +1,11 @@
-export default function sitemap() {
-  const baseUrl = "https://sanepar-site.vercel.app";
-
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/sanepar`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/sanepar-segunda-via`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseUrl}/sanepar-fatura`,
-      lastModified: new Date(),
-    },
-  ];
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://sanepar-site.vercel.app/sitemap.xml",
+  };
 }
